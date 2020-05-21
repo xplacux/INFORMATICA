aebab3ce6b1b32ac4a2e521127b014fd3b33a3fc
Gabriele Placucci <lplacuele02@gmail.com>
2020-05-21 18:30:21 +0200

    first commit

---
+++ Placucci.Gabriele.4H.Rubrica/WpfApp5/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Placucci.Gabriele._4H.Rubrica
+
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Persona p;
+            try
+            {
+                p = new Persona("Martina;Pasquinelli;Rimini;4050;3892996813;5/8/2002");
+
+                MessageBox.Show(p.ToString());
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        private void Button_Click_add(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }   
+
+}
---
+++ Placucci.Gabriele.4H.Rubrica/WpfApp5/models/Persona.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Placucci.Gabriele._4H.Rubrica
+
+
+{
+    class Persona
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Citta { get; set; }
+        public int Cap { get; set; }
+        public string NTelefono { get; set; }
+        public int Eta { get { return DateTime.Now.Year - _dataNascita.Year; } }
+
+        private DateTime _dataNascita;
+
+        //Costruttore di default
+        public Persona()
+        {
+            Nome = "";
+            Cognome = "";
+            Citta = "";
+            NTelefono = "";
+        }
+
+        //Costruttore standard
+        public Persona(string riga)
+        {
+
+            string[] elemeti = riga.Split(';');
+            if (elemeti.Length != 6)
+            {
+                throw new Exception("ERORE:riga csv non valida");
+            }
+            Nome = elemeti[0];
+            Cognome = elemeti[1];
+            Citta = elemeti[2];
+            Cap = Convert.ToInt16(elemeti[3]);
+            NTelefono = elemeti[4];
+            _dataNascita = Convert.ToDateTime(elemeti[5]);
+
+
+        }
+        //metodo molto utile
+        public override string ToString()
+        {
+            return $"{Nome};{Cognome};{Citta};{Cap};{NTelefono};{Eta}";
+        }
+    }
+}
